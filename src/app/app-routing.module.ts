e1f126cae1ce725e0ff893757f3f8554abd53ae9
JonatanV <jonatan.vannberg@elev.ga.ntig.se>
2021-04-14 10:38:46 +0200

    innit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
+import { MapType } from '@angular/compiler';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'map', component: MyMapComponent},
+  {path: '', redirectTo: '/map', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppMapComponent } from './app-map/app-map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppMapComponent
   ],
   imports: [
     BrowserModule,
